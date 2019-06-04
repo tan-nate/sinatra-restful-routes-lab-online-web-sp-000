b260c5febc720e75d0bba9a82b32af418f804b21
Nate Tan <nktan93@gmail.com>
2019-06-04 21:20:34 +0000

    Automatically backed up by Learn

--- db/migrate/20190604211737_ChangeCookTimeToString.rb
+++ db/migrate/20190604211737_ChangeCookTimeToString.rb
@@ -1,4 +1,4 @@
-class ChangeCookTimeToString < ActiveRecord::Migration
+class change_cook_time_to_string < ActiveRecord::Migration
   def change
     change_column :recipes, :cook_time, :string
   end
